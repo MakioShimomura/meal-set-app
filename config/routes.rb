7642756e65c709b450c8c55e5f72ea85abb7bef4
ShimomuraMakio <makky@sonicgarden.jp>
2025-05-02 11:16:56 +0900

    管理者がログインできるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :administrators
+  devise_for :administrators, controllers: { sessions: 'admins/sessions' }, only: %i[sessions]
+  namespace :admins do
+    root 'home#index'
+  end
 end
