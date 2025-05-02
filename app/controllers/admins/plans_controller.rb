468d8f3be4731f3ce34b010d2c9cd175da23601f
ShimomuraMakio <makky@sonicgarden.jp>
2025-05-02 12:21:31 +0900

    管理者がプランを作成できるように

---
+++ app/controllers/admins/plans_controller.rb
@@ -0,0 +1,41 @@
+class Admins::PlansController < ApplicationController
+  before_action :set_plan, only: %i[edit update]
+
+  def index
+    @plans = Plan.default_order
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to admins_plans_path, notice: t('controllers.created')
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @plan.update(plan_params)
+      redirect_to admins_plans_path, notice: t('controllers.updated')
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def set_plan
+    @plan = Plan.find(params[:id])
+  end
+
+  def plan_params
+    params.expect(plan: %i[name])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
                                only: %i[registrations sessions]
   devise_for :administrators, controllers: { sessions: 'admins/sessions' }, only: %i[sessions]
   namespace :admins do
+    resources :plans, only: %i[index new create edit update]
     root 'home#index'
   end
   root 'home#index'
